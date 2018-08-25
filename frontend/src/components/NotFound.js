9437cd02a29b2790b9b4055e50b543f9f800af11
Anshuman <anshu.av97@gmail.com>
2018-08-25 20:02:51 +0530

    Organize filed, added routing

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class NotFound extends Component {
+	render() {
+		return(
+			<div>Probably not the page you're looking for</div>
+		)
+	}
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import App from './components/App';
+import NotFound from './components/NotFound';
+
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<div>
+			<Switch>
+				<Route exact path='/' component={App} />
+				<Route exact path='*' component={NotFound} />
+			</Switch>
+		</div>
+	</BrowserRouter>,
+	document.getElementById('root'));
 registerServiceWorker();
