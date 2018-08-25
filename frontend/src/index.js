1eaf65e3089be54282f0924baf1636817f1e4691
Anshuman <anshu.av97@gmail.com>
2018-08-25 20:20:26 +0530

    Initial redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+import RootReducer from './reducers';
 import App from './components/App';
 import NotFound from './components/NotFound';
 
 import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// Redux Store
+let store = createStore(RootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-	<BrowserRouter>
-		<div>
-			<Switch>
-				<Route exact path='/' component={App} />
-				<Route exact path='*' component={NotFound} />
-			</Switch>
-		</div>
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route exact path='/' component={App} />
+					<Route exact path='*' component={NotFound} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root'));
 registerServiceWorker();
