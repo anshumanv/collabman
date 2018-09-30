a567afe762e511587cf099dcd54e8ffed82dbfd5
Hsankesara <heetsankesara3@gmail.com>
2018-09-30 08:18:41 +0530

    Serialization Added

---
+++ api/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import Profile, Project, DocType, Document, Task, Subtask, SubtaskLog
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'id')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class DocTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DocType
+        fields = '__all__'
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
+    
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class SubtaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtask
+        fields = '__all__'
+
+
+class SubtaskLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubtaskLog
+        fields = '__all__'
