987bfd8a862210d0d47c1f4479ab81e11bcd9bad
Anshuman <anshu.av97@gmail.com>
2018-10-20 17:07:37 +0530

    Handle async action calling

--- frontend/src/actions/projectActions.js
+++ frontend/src/actions/projectActions.js
@@ -4,11 +4,28 @@ import { fetchUserProjects } from '../API/projects';
 
 export const getUserProjects = userId => {
   return (dispatch, getState) => {
-    dispatch({ type: projectActions.FETCH_USER_PROJECT_LOADING });
-    const userProjects = fetchUserProjects(userId);
-    dispatch({
-      type: projectActions.FETCH_USER_PROJECT_DONE,
-      userProjects,
-    });
+    dispatch(projectsLoading());
+    return fetchUserProjects(userId)
+      .then(response => {
+        dispatch(projectsFetched(response.data));
+      })
+      .catch(err => {
+        dispatch(projectsFailed());
+      });
   };
+};
+
+const projectsLoading = () => {
+  return { type: projectActions.FETCH_USER_PROJECT_LOADING, isLoaded: false };
+};
+
+const projectsFetched = projects => {
+  return {
+    type: projectActions.FETCH_USER_PROJECT_DONE,
+    userProjects: projects,
+  };
+};
+
+const projectsLoadingFailed = () => {
+  return { type: projectActions.FETCH_USER_PROJECT_FAILED };
 };
